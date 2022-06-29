f3af2575d5867a8e8ffc2d847715cef7a7f0f63f
hansendx <dhansen@diw.de>
2022-06-29 14:45:25 +0200

    Upgrade from goodtables to frictionless

--- ddionrails_datapackage/validator.py
+++ ddionrails_datapackage/validator.py
@@ -7,8 +7,8 @@ Tabular Data Package.
 
 from typing import Dict
 
-import goodtables.validate
 from datapackage import Package
+from frictionless import validate as _validate
 
 
 def validate(datapackage_location: str, resource_name: str) -> Dict:
@@ -21,12 +21,8 @@ def validate(datapackage_location: str, resource_name: str) -> Dict:
     """
     datapackage = Package(datapackage_location)
     if resource_name not in datapackage.resource_names:
-        message = (
-            f"Resource '{resource_name}' unknown, try: {datapackage.resource_names}"
-        )
+        message = f"Resource '{resource_name}' unknown, try: {datapackage.resource_names}"
         return {"message": message, "valid": False}
     resource = datapackage.get_resource(resource_name)
     schema = resource.schema.descriptor
-    return goodtables.validate(
-        resource.source, schema=schema, order_fields=True, row_limit=-1
-    )
+    return _validate(resource.source, schema=schema)
